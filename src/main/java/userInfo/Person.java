7f2ed9ef6cde80b3189f1cbc65479f2d2e8e740f
handiandi <handiiandii@gmail.com>
2017-05-19 00:57:04 +0200

    Rule-handling added

--- src/main/java/userInfo/Person.java
+++ src/main/java/userInfo/Person.java
@@ -1,6 +1,15 @@
 package userInfo;
 
+import automatization.Rule;
+import auxiliary.Pair;
+
+import java.time.YearMonth;
+import java.time.temporal.WeekFields;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
@@ -13,6 +22,7 @@ public class Person {
     private int id;
     private ArrayList<Ordning> ordninger = new ArrayList<>();
     private ArrayList<Person> ansatte = new ArrayList<>();
+    private ArrayList<Rule> rules = new ArrayList<>();
 
 
     public enum Type{
@@ -112,6 +122,151 @@ public class Person {
         return null;
     }
 
+    public void setRules(ArrayList<Rule> rules){
+        this.rules = rules;
+    }
+
+    public void addRule(Rule rule){
+        this.rules.add(rule);
+
+    }
+
+    public void checkRules(int ordningsID, YearMonth ym){
+        Ordning o = this.getOrdningById(ordningsID);
+        ArrayList<TimeRegistrering> t = o.getTimeRegistreringer(ym);
+
+        HashMap<Pair, Double> weekAccum = new HashMap<>();
+        HashMap<String, Double> monthAccum = new HashMap<>();
+
+        for (Rule r : this.rules) {
+            Rule.AccumType tt = r.getAccumType();
+            for (TimeRegistrering reg : t) {
+                switch (tt){
+                    case Weekly:
+                        weekAccum = this.calcWeeklyRule(weekAccum, reg);
+
+                        break;
+
+                    case Monthly:
+                        monthAccum = this.calcMonthlyRule(monthAccum, reg);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } //for-end
+            System.out.println("antal = " + weekAccum.keySet().size());
+            for (Pair p : weekAccum.keySet()) {
+                System.out.println("Week = " + p.getX()
+                        + ", string = " + p.getY() + ". value = " + weekAccum.get(p));
+            }
+            double limit = r.getLimit();
+            double accepteretSum = 0.0;
+            double afventerSum = 0.0;
+            switch (tt){
+                case Weekly:
+                    int highestWeekNumber = weekAccum.keySet().stream()
+                            .max(Comparator.comparing(Pair::getX)).get().getX();
+                    int lowestWeekNumber = weekAccum.keySet().stream()
+                            .min(Comparator.comparing(Pair::getX)).get().getX();
+
+                    System.out.println("highestWeekNumber = " + highestWeekNumber);
+                    System.out.println("lowestWeekNumber = " + lowestWeekNumber);
+                    ArrayList<Double> accepteret = new ArrayList<>();
+                    ArrayList<Double> afventer = new ArrayList<>();
+                    for (Pair p : weekAccum.keySet()) {
+                        if (p.getY() == "Accepteret"){
+                            accepteret.add(weekAccum.get(p));
+                        }
+                        if (p.getY() == "Afventer"){
+                            accepteret.add(weekAccum.get(p));
+                        }
+
+                    }
+
+                    accepteretSum = accepteret.stream().reduce(0.0, Double::sum);
+                    afventerSum = afventer.stream().reduce(0.0, Double::sum);
+                    System.out.println("accepteretSum = " + accepteretSum);
+                    System.out.println("afventerSum = " + afventerSum);
+                    if (accepteretSum > limit){
+                        System.out.println("Der er flere timer end reglen siger, men timerne er godkendt, så der er ikke noget at gøre");
+
+                        System.out.println("Reglen sagde: " + r.toString());
+                    }
+                    break;
+
+                case Monthly:
+                    accepteretSum = monthAccum.getOrDefault("Accepteret", 0.0);
+                    afventerSum = monthAccum.getOrDefault("Afventer", 0.0);
+                    System.out.println("accepteretSum = " + accepteretSum);
+                    System.out.println("afventerSum = " + afventerSum);
+                    if (accepteretSum > limit){
+                        System.out.println("Der er flere timer end reglen siger, men timerne er godkendt, så der er ikke noget at gøre");
+                        System.out.println("Reglen sagde: " + r.toString());
+                    } else{
+                        System.out.println("Der er " + String.valueOf(limit-accepteretSum) + " timer før grænsen er nået");
+                        System.out.println("Reglen sagde: " + r.toString());
+                    }
+                    //monthAccum = this.calcMonthlyRule(monthAccum, reg);
+                    break;
+
+                default:
+                    break;
+            }
+
+
+
+        } //for-end
+    }
+
+    private HashMap<Pair, Double> calcWeeklyRule(HashMap<Pair, Double> weeklyAccum, TimeRegistrering reg){
+        int weekNumber = reg.getFrom().get(WeekFields.ISO.weekOfWeekBasedYear());
+        switch (reg.getStatus()){
+            case Afvist:
+                break;
+
+            case Afventer:
+                weeklyAccum.put(new Pair(weekNumber, "Afventer"),
+                        weeklyAccum.getOrDefault(new Pair(weekNumber, "Afventer"), 0.0)+reg.getAntalTimer());
+                break;
+            case Ukendt:
+                break;
+
+            case Godkendt:
+            case Accepteret:
+                weeklyAccum.put(new Pair(weekNumber, "Accepteret"),
+                        weeklyAccum.getOrDefault(new Pair(weekNumber, "Accepteret"), 0.0)+reg.getAntalTimer());
+                break;
+
+            default:
+                break;
+        }
+        return weeklyAccum;
+    }
+
+    private HashMap<String, Double> calcMonthlyRule(HashMap<String, Double> monthAccum, TimeRegistrering reg){
+        switch (reg.getStatus()){
+            case Afvist:
+                break;
+
+            case Afventer:
+                monthAccum.put("Afventer", monthAccum.getOrDefault("Afventer", 0.0)+reg.getAntalTimer());
+                break;
+            case Ukendt:
+                break;
+
+            case Godkendt:
+            case Accepteret:
+                monthAccum.put("Accepteret", monthAccum.getOrDefault("Accepteret", 0.0)+reg.getAntalTimer());
+                break;
+
+            default:
+                break;
+        }
+        return monthAccum;
+    }
+
     @Override
     public String toString() {
         String s = this.name;
@@ -134,15 +289,17 @@ public class Person {
     }
 
     public String toStringOrdninger() {
+        if (this.ordninger.size() == 0)
+            return "Ingen ordninger";
         return this.ordninger.stream().map(Object::toString)
                 .collect(Collectors.joining("\n"));
-
     }
 
     public String toStringAnsatte() {
+        if (this.ansatte.size() == 0)
+            return "Ingen ansatte";
         return this.ansatte.stream().map(Object::toString)
                 .collect(Collectors.joining("\n"));
-
     }
 
 }
