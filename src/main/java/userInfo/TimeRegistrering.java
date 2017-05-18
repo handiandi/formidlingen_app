504727d07ce6764f41a2d4eaf2099355c4a0e4f0
handiandi <handiiandii@gmail.com>
2017-05-19 00:59:57 +0200

    Using Builder pattern. Has added values to one of the enums and added a new, local enum type to the other ('Ny' - 'New')

--- src/main/java/userInfo/TimeRegistrering.java
+++ src/main/java/userInfo/TimeRegistrering.java
@@ -3,39 +3,100 @@ package userInfo;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
-import java.time.chrono.ChronoPeriod;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by anders-dev on 5/9/17.
  */
 public class TimeRegistrering {
+    private int id;
     private LocalDateTime from;
     private LocalDateTime to;
+    private String ansat;
     private String author;
     private Status status;
     private TimeType type;
     private double antalTimer;
+    private static final Map<Integer, TimeType> intToTimeType
+            = new HashMap<Integer, TimeType>();
+
+    static {
+        for (TimeType type : TimeType.values()){
+            intToTimeType.put(type.value, type);
+        }
+    }
 
     public enum Status{
-        Godkendt, Afvendtende, Afvist
+        Ukendt, Accepteret, Afventer, Afvist, Godkendt, Ny; //Ny = nyoprettet, klar til at blive gemt/indtastet
     }
 
     public enum TimeType{
-        Almindelig, Sygdom, Personalemoede,
-        BarnSyg, Foelgevagt, Puljetimer, MUSsamtale
+        Ukendt(-1), Almindelig(0), Sygdom(1), Personalemoede(2),
+        BarnSyg(5), Foelgevagt(9), Puljetimer(10), MUSsamtale(13);
+
+        private final int value;
+
+        TimeType(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
-    public TimeRegistrering(LocalDateTime from, LocalDateTime to,
-                            String author, Status status, TimeType type){
-        this.from = from;
-        this.to = to;
-        this.author = author;
-        this.status = status;
-        this.type = type;
-        this.antalTimer = calcAntalTimer(from, to);
+
+
+
+    public static class Builder{
+        //Required parameters
+
+        private final LocalDateTime from;
+        private final LocalDateTime to;
+        private final String ansat;
+
+        //Optional parameters
+        private int id ;
+        private String author = "";
+        private Status status = Status.Ukendt;
+        private TimeType type = TimeType.Ukendt;
+        private double antalTimer = 0.0;
+
+        public Builder(LocalDateTime from, LocalDateTime to, String ansat){
+            this.from = from;
+            this.to = to;
+            this.ansat = ansat;
+        }
+
+        public Builder author(String author){this.author = author; return this;}
+        public Builder status(Status status){this.status = status; return this;}
+        public Builder type(TimeType type){this.type = type; return this;}
+        public Builder id(int id){this.id = id; return this;}
+
+        public TimeRegistrering build(){
+            return new TimeRegistrering(this);
+        }
+    } //end Builder class
+    private TimeRegistrering(Builder builder){
+        this.id = this.generateId(builder.id);
+        this.from = builder.from;
+        this.to = builder.to;
+        this.ansat = builder.ansat;
+        this.status = builder.status;
+        this.type = builder.type;
+        this.author = builder.author;
+        this.antalTimer = calcAntalTimer(this.from, this.to);
 
     }
 
+
+    private int generateId(int id){
+        if (id != 0)
+            return id;
+        return ThreadLocalRandom.current().nextInt(-10*(int)Math.pow(10.0,6.0), 0);
+    }
     private double calcAntalTimer(LocalDateTime from, LocalDateTime to){
         double m = ChronoUnit.MINUTES.between(from, to);
         int timer = (int) m/60;
@@ -43,6 +104,13 @@ public class TimeRegistrering {
         double t = 1.0/(60.0/(double)mLeft);
         return (double) timer + t;
 
+    }
+
+    public TimeType getTimeTypeFromInt(int i){
+        TimeType type = intToTimeType.get(Integer.valueOf(i));
+        if (type == null)
+            return TimeType.Ukendt;
+        return type;
     }
 
     public YearMonth getYearMonth(){
@@ -64,11 +132,19 @@ public class TimeRegistrering {
         return this.status;
     }
 
-    public String getAuthor() {
-        return this.author;
+    public String getAnsat() {
+        return this.ansat;
     }
 
     public TimeType getType() {
         return this.type;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getId() {
+        return id;
     }
 }
